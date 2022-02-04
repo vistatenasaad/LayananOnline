92d0403a57790c2cb673fcedfa841fc179ddc5b0
sellauno <sellauno03@gmail.com>
2022-02-04 11:19:47 +0700

    Tracking

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -217,56 +217,56 @@
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Perijinan</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rptka2')}}">Rencana Penggunaan Tenaga Kerja Asing</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('imtka2')}}">Izin Menggunakan Tenaga Kerja Asing</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('dkpoa2')}}">Dana Kompensasi Pengggunaan Orang Asing</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('vtt2')}}">Visa Tinggal Terbatas</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('kitp2')}}">Kartu Izin Tinggal Tetap</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('naturalisasi2')}}">Naturalisasi</a></li>
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rptka1')}}">Rencana Penggunaan Tenaga Kerja Asing</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('imtka1')}}">Izin Menggunakan Tenaga Kerja Asing</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('dkpoa1')}}">Dana Kompensasi Pengggunaan Orang Asing</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('vtt1')}}">Visa Tinggal Terbatas</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('kitp1')}}">Kartu Izin Tinggal Tetap</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('naturalisasi1')}}">Naturalisasi</a></li> 
                         </ul>
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Perizinan</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijinpm2')}}">Ijin Penelitian Madrasah</a></li>  -->
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijoptpq2')}}">Ijop TPQ</a></li>  -->
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijopm2')}}">Ijop Madin</a></li>  -->
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijopp2')}}">Ijop Pontren</a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijinpm1')}}">Ijin Penelitian Madrasah</a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijoptpq1')}}">Ijop TPQ</a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijopm1')}}">Ijop Madin</a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ijopp1')}}">Ijop Pontren</a></li>  -->
                         </ul>
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Persetujuan</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('propbos2')}}">Proposal BOS</a></li>  -->
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('blankokua2')}}">Permohonan Blanko KUA</a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('propbos1')}}">Proposal BOS</a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('blankokua1')}}">Permohonan Blanko KUA</a></li>  -->
                         </ul>
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Rekomendasi</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ripri2')}}">Rekomendasi Ijin Pendirian Rumah Ibadah</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rbm2')}}">Rekom Bantuan Masjid</a></li>
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rskmt2')}}">Rekom Surat Keterangan Majelis Taklim</a></li>  -->
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rpm2')}}">Rekom Pindah Madrasah</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('prsln2')}}">Pengantar Rekom Studi Luar Negeri</a></li>
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rbponpes2')}}">Rekom Bantuan Ponpes/Madin</a></li>  -->
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rbmf2')}}">Rekom Bantuan Madrasah Formal </a></li>  -->
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ritas2')}}">Rekom ITAS</a></li>  -->
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rpu2')}}">Rekom Paspor Umroh</a></li>
-                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rph2')}}">Rekom Paspor Haji</a></li>
-                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ppm2')}}">Piagam Pendirian Masjid</a></li>  -->
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ripri1')}}">Rekomendasi Ijin Pendirian Rumah Ibadah</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rbm1')}}">Rekom Bantuan Masjid</a></li> 
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rskmt1')}}">Rekom Surat Keterangan Majelis Taklim</a></li>  -->
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rpm1')}}">Rekom Pindah Madrasah</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('prsln1')}}">Pengantar Rekom Studi Luar Negeri</a></li> 
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rbponpes1')}}">Rekom Bantuan Ponpes/Madin</a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rbmf1')}}">Rekom Bantuan Madrasah Formal </a></li>  -->
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ritas1')}}">Rekom ITAS</a></li>  -->
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rpu1')}}">Rekom Paspor Umroh</a></li> 
+                            <li><i class="fa fa-puzzle-piece"></i><a href="{{route('rph1')}}">Rekom Paspor Haji</a></li> 
+                            <!-- <li><i class="fa fa-puzzle-piece"></i><a href="{{route('ppm1')}}">Piagam Pendirian Masjid</a></li>  -->
                         </ul>
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Pelayanan Lain yang Diperlukan</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <!-- <li><i class="fa fa-table"></i><a href="{{route('sijazah2')}}">Suket Ijazah</a></li> -->
+                            <!-- <li><i class="fa fa-table"></i><a href="{{route('sijazah1')}}">Suket Ijazah</a></li> -->
                         </ul>
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Pelayanan yang Diperlukan</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <!-- <li><i class="fa fa-table"></i><a href="{{route('mjamaah2')}}">Mutasi Jamaah</a></li> -->
+                            <!-- <li><i class="fa fa-table"></i><a href="{{route('mjamaah1')}}">Mutasi Jamaah</a></li> -->
                         </ul>
                     </li>
                 </ul>
@@ -291,77 +291,13 @@
                 <div class="col-sm-7">
                     <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
                     <div class="header-left">
-                        <button class="search-trigger"><i class="fa fa-search"></i></button>
+                        <!-- <button class="search-trigger"><i class="fa fa-search"></i></button>
                         <div class="form-inline">
                             <form class="search-form">
                                 <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
                                 <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
                             </form>
-                        </div>
-
-                        <div class="dropdown for-notification">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-bell"></i>
-                                <span class="count bg-danger">5</span>
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="notification">
-                                <p class="red">You have 3 Notification</p>
-                                <a class="dropdown-item media bg-flat-color-1" href="#">
-                                    <i class="fa fa-check"></i>
-                                    <p>Server #1 overloaded.</p>
-                                </a>
-                                <a class="dropdown-item media bg-flat-color-4" href="#">
-                                    <i class="fa fa-info"></i>
-                                    <p>Server #2 overloaded.</p>
-                                </a>
-                                <a class="dropdown-item media bg-flat-color-5" href="#">
-                                    <i class="fa fa-warning"></i>
-                                    <p>Server #3 overloaded.</p>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="dropdown for-message">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="message" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="ti-email"></i>
-                                <span class="count bg-primary">9</span>
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="message">
-                                <p class="red">You have 4 Mails</p>
-                                <a class="dropdown-item media bg-flat-color-1" href="#">
-                                    <span class="photo media-left"><img alt="avatar" src="images/avatar/1.jpg"></span>
-                                    <span class="message media-body">
-                                        <span class="name float-left">Jonathan Smith</span>
-                                        <span class="time float-right">Just now</span>
-                                        <p>Hello, this is an example msg</p>
-                                    </span>
-                                </a>
-                                <a class="dropdown-item media bg-flat-color-4" href="#">
-                                    <span class="photo media-left"><img alt="avatar" src="images/avatar/2.jpg"></span>
-                                    <span class="message media-body">
-                                        <span class="name float-left">Jack Sanders</span>
-                                        <span class="time float-right">5 minutes ago</span>
-                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                                    </span>
-                                </a>
-                                <a class="dropdown-item media bg-flat-color-5" href="#">
-                                    <span class="photo media-left"><img alt="avatar" src="images/avatar/3.jpg"></span>
-                                    <span class="message media-body">
-                                        <span class="name float-left">Cheryl Wheeler</span>
-                                        <span class="time float-right">10 minutes ago</span>
-                                        <p>Hello, this is an example msg</p>
-                                    </span>
-                                </a>
-                                <a class="dropdown-item media bg-flat-color-3" href="#">
-                                    <span class="photo media-left"><img alt="avatar" src="images/avatar/4.jpg"></span>
-                                    <span class="message media-body">
-                                        <span class="name float-left">Rachel Santos</span>
-                                        <span class="time float-right">15 minutes ago</span>
-                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
 
